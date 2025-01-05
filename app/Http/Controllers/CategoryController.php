697ec9cb85fcda9848db6b6994b3b3a55fc3da8c
ismail <ismailouabderrazak@gmail.com>
2025-01-05 17:39:31 +0100

    finish categoryController files

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Category;
 class CategoryController extends Controller
 {
-    
+    // for converting the data to json format
+    public function index(){
+
+        $categories = Category::all();
+        return response()->json($categories);
+    }
+    // for storing the data
+    public function store(Request $request){
+
+        $validated = $request->validate(['name' => 'required|string|max:255']);
+        $category = Category::create($validated);
+
+        return response()->json($category, 201);
+    }
+    // for showing the data
+    public function show(Category $category){
+
+        return response()->json($category);
+    }
+    // for updating the data
+    public function update(Request $request, Category $category){
+
+        $validated = $request->validate(['name' => 'required|string|max:255']);
+        $category->update($validated);
+        return response()->json($category);
+    }
+    // for deleting the data
+    public function destroy(Category $category){
+
+        $category->delete();
+        return response()->json(['message' => 'Category deleted successfully']);
+    }
 }
